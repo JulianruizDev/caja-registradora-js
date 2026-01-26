36fe8032ddd6b2f1fe0f0caabbcf737d40f183f3
Julian Ruiz <julianruiza4@gmail.com>
2026-01-25 22:07:11 -0500

    refactor: separar cálculo del total en función independiente

--- subtotal.js
+++ subtotal.js
@@ -6,6 +6,12 @@ function calcularSubtotal(precios) {
     return total;
 }
 
+function calcularSubtotal(precios) {
+    let total = calcularSubtotal(precios);
+    let conIVA = total * 1.19;
+    return conIVA;
+}
+
 // Prueba simple
 let compra = [3000, 4500, 2000];
 console.log(calcularSubtotal(compra)); // Debería imprimir 9500
