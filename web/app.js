7e76b61a7d5b9d5e72c17b55ecfd83ac631c3d79
Julian Ruiz <julianruiza4@gmail.com>
2026-01-30 01:05:39 -0500

    feat: inicializar estado y conexión entre HTML y JavaScript

---
+++ web/app.js
@@ -0,0 +1,9 @@
+//Estado de la aplicación
+let precios = [];
+
+// Puente entre la interfaz y la lógica
+const inputPrecio = document.getElementById("precio");
+const botonAgregar = document.getElementById("agregar");
+const botonTicket = document.getElementById("ticket");
+const lista = document.getElementById("lista");
+const resultado = document.getElementById("resultado");
